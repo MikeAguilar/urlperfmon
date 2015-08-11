6c77644454faa0356d858d493722e9df9eb77d45
Mike Aguilar <miguelaguilar79@gmail.com>
2015-08-10 18:49:43 -0700

    Added function downloadFile(url) and the HEAD request to get initial URL info.

--- urlperfmon.py
+++ urlperfmon.py
@@ -1,5 +1,45 @@
 #!/usr/bin/python
+# 
+# Author:    Miguel Aguilar
+# Project:   URL performance monitor with Local and Origin GeoLocation info
+# Location:  Seattle, WA. Aug 5, 2015
+# Script:    ip2geo.py
+# Logs:      ./geoInfo.log
 #
-# Project:    URL Performance Monitor
-# Author:     Miguel Aguilar
 
+import time
+import urllib2
+import ast
+import sys
+
+### Download only 1MB from the URL provided
+def downloadFile(url):
+    ### Do HEAD request to get info about the file
+    start = time.time()
+    req = urllib2.Request(url)
+    req.get_method = lambda : 'HEAD'
+    response = urllib2.urlopen(req)
+    # total_length = response.info()
+    total_length = 0
+    print ''
+    print response.info()
+    print ('Elapsed time: ' + str(time.time()-start))
+    # print (type(response.info()))
+
+    ### Define headers for request
+    headers = { 'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)-MikeBoss', \
+                'Range' : (str(total_length-104857600)+':')  }
+                
+                
+def Main():
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    else:
+        url = raw_input("Enter a valid URL: ")
+        
+    downloadFile(url)
+
+if __name__ == '__main__':
+    Main()
+    
+    
