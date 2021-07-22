6fbd38f943014b50823ca7a54aba6c9d45476078
Mike Aguilar <miguelaguilar79@gmail.com>
2021-07-21 19:27:12 -0700

    Updated code to run on Python 3.7
    Changed from using old urllib2, to using the new urllib.request library.
    
    Added some debug lines and assigned a default URL in case none is passed to the script as an argument from the command line.

--- urlperfmon.py
+++ urlperfmon.py
@@ -1,15 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # 
+# Re-writen for Python v3.7 on 7/21/2021
 # Author:    Miguel Aguilar
-# Project:   URL performance monitor with Local and Origin GeoLocation info
+# Project:   URL performance monitor
 # Location:  Seattle, WA. Aug 5, 2015
 # Script:    urlperfmon.py
 # Output:    ./urlperfmon.csv
+# Syntax: urlperfmon.py http://some.url/to/download/a/file
+
 
 
 import time
-import urllib2
-import ast
+import urllib.request
+#import ast
 import sys
 
 
@@ -17,27 +20,24 @@ import sys
 def downloadFile(url):
     ### Do HEAD request to get info about the file
     start = time.time()
-    req = urllib2.Request(url)
-    req.get_method = lambda : 'HEAD'
-    response = urllib2.urlopen(req)
-    total_length = int(response.info().getheader('Content-Length'))
-    print '\n\rHEAD Request response headers: '
-    print response.info()
+    req = urllib.request.Request(url, headers={'User-Agent':"Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}, method='HEAD')
+    response = urllib.request.urlopen(req)
+    #print ("Response <Content-Length>: ", response.info()['Content-Length']) #DEBUG#
+    total_length = int(response.info()['Content-Length'])
+    print ('\n\rHEAD Request response headers: ')
+    print (response.info())
     print ('HEAD Request Elapsed time: ' + str(time.time()-start))
     # print (type(response.info()))
 
-    ### Execute the byte-range GET request to download only the last 1MB of the file
+    ### Execute the byte-range GET request to download only the last 10MB of the file
     # data_list = []   #This is in case you want to save the data downloaded
     bytes_downloaded = 0
     start = time.time()
-    
-    ### Range is only 10MB from the end of the file
-    range = 'bytes=' + str(int(total_length)-10485760)+'-'+ str(total_length)
-    print range   #Debug-line
-    req = urllib2.Request(url)
-    req.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)-MikeBoss')
-    req.add_header('Range', range)
-    response = urllib2.urlopen(req)
+    range = 'bytes=' + str(int(total_length)-10485760)+'-'+ str(total_length)     ### Range is only 10MB from the end of the file
+    print (range)   #DEBUG#
+    req = urllib.request.Request(url, headers={'User-Agent':"Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11", 'Range': range})
+    response = urllib.request.urlopen(req)
+    #print ("Response: ", response.info()) #DEBUG#
     while 1:
         data = response.read(1024)
         bytes_downloaded += len(data)
@@ -50,21 +50,22 @@ def downloadFile(url):
     endtime = time.time()
     elapsed = endtime-start
     dlspeed = (bytes_downloaded*8/1048576)/elapsed
-    print "Downloaded: " + str(round((bytes_downloaded/1048576), 2)) + " MBytes"#str(totaldl) +' MBytes'
-    # print "\nMBytes downloaded: " + str((int(total_length)-(int(total_length)-(10485760))/1048576))
-    print "Download Speed: " + str(round(dlspeed, 2)) + ' Mbps' #str(((bytes_downloaded*8)/(1048576))/(time.time() - start))
-    print "Time elapsed: " + str(round((endtime-start), 2)) + ' Seconds' #str(round((time.time()-start), 2)) + " seconds"
+    print ("Downloaded: " + str(round((bytes_downloaded/1048576), 2)) + " MBytes")  #str(totaldl) +' MBytes'
+    # print ("\nMBytes downloaded: " + str((int(total_length)-(int(total_length)-(10485760))/1048576)))
+    print ("Download Speed: " + str(round(dlspeed, 2)) + ' Mbps') #str(((bytes_downloaded*8)/(1048576))/(time.time() - start))
+    print ("Time elapsed: " + str(round((endtime-start), 2)) + ' Seconds') #str(round((time.time()-start), 2)) + " seconds"
                 
-def Main():
+def main():
     if len(sys.argv) > 1:
         url = sys.argv[1]
     else:
-        url = raw_input("Enter a valid URL: ")
+        print ('usage: ', sys.argv[0] + 'http://valid.url/to/download/file')
+        url = "http://ipv4.download.thinkbroadband.com/512MB.zip" #Setting this URL as default
+        #url = raw_input("Enter a valid URL: ")
         
     downloadFile(url)
 
 
 if __name__ == '__main__':
-    Main()
-    
+    main()
     
